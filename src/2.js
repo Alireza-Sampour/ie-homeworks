e80b99287ad329b9badb332ad09f5fa9d50e8e2a
Alireza <Alirezasampoor@gmail.com>
2021-10-23 19:32:43 +0330

    upload files

---
+++ src/2.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        const colors = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed ", "Indigo  ", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"];
+        let style = {fontSize: 20};
+        return (
+            <div id='inner_div'>
+                <h1>Hello, World!</h1>
+                <label htmlFor='drop_down' style={style}>What color?</label><br/>
+                <select id='drop_down' title='Choose color' data-live-search='true' className='selectpicker show-tick' onChange={() => {document.getElementById('root').style='background-color: ' + document.getElementById('drop_down').value; document.getElementById('inner_div').style.color=((document.getElementById('drop_down').value === 'Black') ? 'White' : 'Black')}}>
+                    {colors.map(function(color, i){
+                        return <option style={{background: color}} value={color} key={i}>{color}</option>;
+                    })}
+                </select>
+            </div>
+        );
+    }
+}
+
+document.title = "Color Picker"
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Question2 from './2.js';
+  
+let destination = document.querySelector("#container")
+  
+ReactDOM.render(
+    <div>
+     <Question2/>
+    </div>,
+    destination
+);
